086ab921ff6e6012a914e8623f5c3f61744c707a
Mario Zancanaro <nano@creaator.com>
2016-07-11 17:25:33 +0200

    Update site layout

--- website/www/urls.py
+++ website/www/urls.py
@@ -8,6 +8,18 @@ urlpatterns = [
 
     #/index
     url(r'^index/$', views.index,  name='index'),
+    #/overview
+    url(r'^overview/$', views.overview, name='overview'),
     #/source
     url(r'^source/$', views.source, name='source'),
+    #/get_started
+    url(r'^get_started/$', views.get_started, name='get_started'),
+    #/api
+    url(r'^api/$', views.api, name='api'),
+    #/download
+    url(r'^download/$', views.download, name='download'),
+    #/contact
+    url(r'^contact/$', views.contact, name='contact'),
+    #/manual
+    url(r'^manual/$', views.manual, name='manual'),
 ]
--- website/www/views.py
+++ website/www/views.py
@@ -7,7 +7,43 @@ def index(request):
     return render(request, 'layout/body_face.html', {'content':body})
 
 
+def overview(request):
+    title = "overview"
+    body = markdownify("overview")
+    return render(request, 'layout/body_inside.html', {'content':body, 'title':title})
+
+
 def source(request):
     title = "source code"
     body = markdownify("source")
+    return render(request, 'layout/body_inside.html', {'content':body, 'title':title})
+
+
+def get_started(request):
+    title = "get started"
+    body = markdownify("get_started")
+    return render(request, 'layout/body_inside.html', {'content':body, 'title':title})
+
+
+def api(request):
+    title = "API"
+    body = markdownify("api")
+    return render(request, 'layout/body_inside.html', {'content':body, 'title':title})
+
+
+def download(request):
+    title = "download"
+    body = markdownify("download")
+    return render(request, 'layout/body_inside.html', {'content':body, 'title':title})
+
+
+def contact(request):
+    title = "contact"
+    body = markdownify("contact")
+    return render(request, 'layout/body_inside.html', {'content':body, 'title':title})
+
+
+def manual(request):
+    title = "manual"
+    body = markdownify("manual")
     return render(request, 'layout/body_inside.html', {'content':body, 'title':title})
